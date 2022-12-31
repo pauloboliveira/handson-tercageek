bb3fea728d5793c63687d6553f2e9db3ef7caea5
pauloboliveira <pborges086@hotmail.com>
2022-12-31 15:53:03 -0300

    Layout informações da tela

--- lib/home.dart
+++ lib/home.dart
@@ -70,6 +70,7 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
+          SizedBox(height: 15,),
           _info(),
         ],
       ),
@@ -92,44 +93,91 @@ class _HomePageState extends State<HomePage> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _labelText('MARCA'),
-        Container(
-          width: double.infinity,
-          child: const Card(
-            elevation: 0,
-            color: Color(0xFFEDEAEA),
-            shape: OutlineInputBorder(
-              borderSide: BorderSide(color: Color(0xFFEDEAEA)),
-              borderRadius: BorderRadius.all(Radius.circular(5)),
+        _cardInfo('Editora Panini'),
+        _labelText('TÍTULO'),
+        _cardInfo('Livro tal'),
+        SizedBox(height: 15,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(child: _labelText('PESO')),
+            SizedBox(width: 10,),
+            Expanded(child: _labelText('ALTURA')),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: _containerInfo('1')
             ),
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Editora Panini',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+            SizedBox(width: 10,),
+            Expanded(
+              child: _containerInfo('1')
+            ),
+          ],
+        ),
+        SizedBox(height: 10,),
+        Row(
+          children: [
+            Expanded(child: _labelText('LARGURA')),
+            SizedBox(width: 10,),
+            Expanded(child: _labelText('PROFUNDIDADE')),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: _containerInfo('1')
+            ),
+            SizedBox(width: 10,),
+            Expanded(
+              child: _containerInfo('1')
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  _containerInfo(text) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFFEDEAEA),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontFamily: 'Poppins',
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  _cardInfo(String text) {
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        elevation: 0,
+        color: const Color(0xFFEDEAEA),
+        shape: const OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFEDEAEA)),
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            overflow: TextOverflow.fade,
+            text,
+            style: const TextStyle(
+              fontFamily: 'Poppins',
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
-        _labelText('TÍTULO'),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _labelText('PESO'),
-            _labelText('ALTURA'),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _labelText('LARGURA'),
-            _labelText('PROFUNDIDADE'),
-          ],
-        )
-      ],
+      ),
     );
   }
 }
